42a9d91f6d331844daf993a0827e19a18f675c4c
Sven Burkard <dev@sven-burkard.de>
2023-12-12 12:21:36 +0100

    client : simplified and working now by switching to NewCheckGroup instead of NewListWithData and NewCheck

--- client/main.go
+++ client/main.go
@@ -4,12 +4,9 @@ import (
     "fmt"
     "os"
 
-    "tag/models"
-
     "fyne.io/fyne/v2"
     "fyne.io/fyne/v2/app"
     "fyne.io/fyne/v2/container"
-    "fyne.io/fyne/v2/data/binding"
     "fyne.io/fyne/v2/widget"
     "fyne.io/fyne/v2/layout"
 )
@@ -40,30 +37,30 @@ func main() {
     )
 
 
-
     a := app.New()
     w := a.NewWindow("photoFS Tag App")
 
-    w.Resize(fyne.NewSize(1280, 1280))
+//     w.Resize(fyne.NewSize(1280, 1280))
+    w.Resize(fyne.NewSize(1280, 600))
     w.CenterOnScreen()
 
-    data := []models.Tag{
-        models.NewTag("party/birthday/smith/steven"),
-        models.NewTag("party/birthday/smith/julia"),
-        models.NewTag("party/christmas/company"),
+    tagsList := []string{
+      "party/birthday/smith/steven",
+      "party/birthday/smith/julia",
+      "party/christmas/company",
     }
 
-    tags := binding.NewUntypedList()
-    for _, t := range data {
-      tags.Append(t)
-    }
+    tags := widget.NewCheckGroup(tagsList, func(selectedTags []string) {
+      fmt.Println("tags check_group:")
+      fmt.Println("  selected tags: ", selectedTags)
+    })
 
 
     newtagNameTxt := widget.NewEntry()
     newtagNameTxt.PlaceHolder = "New Tag..."
 
     addBtn := widget.NewButton("Add", func() {
-      tags.Append(models.NewTag(newtagNameTxt.Text))
+      tags.Append(newtagNameTxt.Text)
       newtagNameTxt.Text = ""
     })
     addBtn.Disable()
@@ -75,94 +72,6 @@ func main() {
             addBtn.Enable()
         }
     }
-
-
-    selectedTags := widget.NewListWithData(
-        // the binding.List type
-        tags,
-        // func that returns the component structure of the List Item
-        // exactly the same as the Simple List
-        func() fyne.CanvasObject {
-            return container.NewBorder(
-              nil, // TOP
-              nil, // BOTTOM
-              nil, // Left
-              // LEFT
-              widget.NewCheck("", func(b bool) {}),
-              // REST
-              widget.NewLabel(""),
-            )
-        },
-        // func that is called for each item in the list and allows
-        // but this time we get the actual DataItem we need to cast
-        func(di binding.DataItem, o fyne.CanvasObject) {
-            ctr, _ := o.(*fyne.Container)
-            // ideally we should check `ok` for each one of those casting
-            // but we know that they are those types for sure
-            l := ctr.Objects[0].(*widget.Label)
-            c := ctr.Objects[1].(*widget.Check)
-            /*
-              diu, _ := di.(binding.Untyped).Get()
-              tag := diu.(models.Tag)
-            */
-            tag := models.NewTagFromDataItem(di)
-            l.SetText(tag.Name)
-            c.SetChecked(tag.Selected)
-
-//             fmt.Println(tag.Selected)
-
-            if(tag.Selected == true){
-                fmt.Println(tag.Name)
-            }
-        },
-    )
-
-    tagBtn := widget.NewButton("tag    (with selected tags and files)", func() {
-//       tags.Append(models.NewTag(newtagNameTxt.Text))
-//       fmt.Printf("Type of Arg = %s\n", newtagNameTxt.Text)
-//       fmt.Println(tags.Text)
-//       newtagsNameTxt.Text = ""
-
-//         fmt.Println(selectedTags)
-
-//         // Does not work
-//         for _, tag := range tags {
-//             if(tag.Selected == true){
-//                 fmt.Println(tag.Name)
-//             }
-//         }
-
-//         // Works, but only gives the initial data, not any changes
-//         for _, tag := range data {
-//             fmt.Println(tag)
-//         }
-
-//         fmt.Println(tags.Get())
-
-//////////////////////
-        list, _ := tags.Get()
-//         fmt.Println(list)
-
-        for _, tag := range list {
-//             fmt.Println(tag)
-            fmt.Println(tag)
-        }
-//////////////////////
-
-    })
-
-    delBtn := widget.NewButton("delete selected tags", func() {
-//       tags.Append(models.NewTag(newtagNameTxt.Text))
-      newtagNameTxt.Text = ""
-//////////////////////
-      // to remove all tags
-      list, _ := tags.Get()
-      list = list[:0]
-      fmt.Println(list)
-      tags.Set(list)
-//////////////////////
-    })
-
 
 
 
@@ -224,7 +133,7 @@ func main() {
                             newtagNameTxt,
                         ),
                     ),
-                    delBtn, // BOTTOM
+                    nil, // BOTTOM
                     nil, // Right
                     nil, // Left
                     nil, // REST
@@ -239,14 +148,14 @@ func main() {
                     layout.NewGridLayout(1),
                     container.New(
                         layout.NewGridLayout(4),
-                        selectedTags,
+                        tags,
                         widget.NewLabel("placeholder"),
                         widget.NewLabel("placeholder"),
                         widget.NewLabel("placeholder"),
                     ),
                     container.NewBorder(
                         // TOP
-                        tagBtn,
+                        nil,
                         nil, // BOTTOM
                         nil, // RIGHT
                         nil, // LEFT
